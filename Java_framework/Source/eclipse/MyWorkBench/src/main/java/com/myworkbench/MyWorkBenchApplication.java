da8b12a13bf435dbd3c653af64ec5688888f0aea
JunoYahata <juno.yahata@seed-it.jp>
2024-09-11 08:26:47 +0900

    2024-09-10

---
+++ Java_framework/Source/eclipse/MyWorkBench/src/main/java/com/myworkbench/MyWorkBenchApplication.java
@@ -0,0 +1,13 @@
+package com.myworkbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWorkBenchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWorkBenchApplication.class, args);
+	}
+
+}
---
+++ Java_framework/Source/eclipse/MyWorkBench/src/test/java/com/myworkbench/MyWorkBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myworkbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWorkBenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
