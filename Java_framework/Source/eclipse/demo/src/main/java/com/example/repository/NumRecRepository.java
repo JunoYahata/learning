119a16590227620c05ac49bbf0711d3d2fc9338d
JunoYahata <juno.yahata@seed-it.jp>
2024-09-18 21:31:12 +0900

    Update NumRecRepository.java

--- Java_framework/Source/eclipse/demo/src/main/java/com/example/repository/NumRecRepository.java
+++ Java_framework/Source/eclipse/demo/src/main/java/com/example/repository/NumRecRepository.java
@@ -2,9 +2,9 @@ package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.model.NumRecDTO;
+import com.example.model.NumRec;
 
-public interface  NumRecRepository extends JpaRepository<NumRecDTO, Integer> {
+public interface  NumRecRepository extends JpaRepository<NumRec, Integer> {
 
 
 }
